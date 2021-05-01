a2b98cda5908582ff2189be8c01a5b5364c153c2
Dinesh Kumar <dseera6@gmail.com>
2021-05-02 04:17:15 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -18,5 +18,8 @@ plt.ylabel('Marks %')
 plt.show()
 
 #classification of data for in and out
-X = s_data.iloc[:, :-1].values
+x = s_data.iloc[:, :-1].values
 y = s_data.iloc[:, 1].values
+
+from sklearn.model_selection import train_test_split  
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state=0)
