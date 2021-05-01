a14f5767967ca8fe7176acef11cc0f3071678fc4
Dinesh Kumar <dseera6@gmail.com>
2021-05-02 03:38:32 +0530

    plotting
    Signed-off-by: Dinesh Kumar <dseera6@gmail.com>

--- predict.py
+++ predict.py
@@ -5,11 +5,14 @@ import matplotlib.pyplot as plt
 
 #read DataSet
 addr = "student_scores.csv"
-
 s_data = pd.read_csv(addr)
-
 print("Data imported successfully")
-
 dataS = s_data.head(10)
-
 print(dataS)
+
+#plotting score distribution
+s_data.plot(x='Hours', y='Scores', style='o')
+plt.title('Studied Hours Graph')
+plt.xlabel('Hours')
+plt.ylabel('Marks %')
+plt.show()
