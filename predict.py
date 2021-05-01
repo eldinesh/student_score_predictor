bff8be736f844253698a7fb8d567df8003abe7ae
Dinesh Kumar <dseera6@gmail.com>
2021-05-02 03:00:41 +0530

    import data
    Signed-off-by: Dinesh Kumar <dseera6@gmail.com>

--- predict.py
+++ predict.py
@@ -3,3 +3,13 @@ import pandas as pd
 import numpy as np 
 import matplotlib.pyplot as plt
 
+#read DataSet
+addr = "student_scores.csv"
+
+s_data = pd.read_csv(addr)
+
+print("Data imported successfully")
+
+dataS = s_data.head(10)
+
+print(dataS)
