249fb259c20711d562b3767f6a2c16ea4bcf9874
Dinesh Kumar <dseera6@gmail.com>
2021-05-02 04:26:46 +0530

    training
    Signed-off-by: Dinesh Kumar <dseera6@gmail.com>

--- predict.py
+++ predict.py
@@ -8,14 +8,14 @@ addr = "student_scores.csv"
 s_data = pd.read_csv(addr)
 print("Data imported successfully")
 dataS = s_data.head(10)
-print(dataS)
+#print(dataS)
 
 #plotting score distribution
 s_data.plot(x='Hours', y='Scores', style='o')
 plt.title('Studied Hours Graph')
 plt.xlabel('Hours')
 plt.ylabel('Marks %')
-plt.show()
+#plt.show()
 
 #classification of data for in and out
 x = s_data.iloc[:, :-1].values
@@ -23,3 +23,9 @@ y = s_data.iloc[:, 1].values
 
 from sklearn.model_selection import train_test_split  
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state=0)
+
+#Training Algorithm
+from sklearn.linear_model import LinearRegression  
+regressor = LinearRegression()  
+regressor.fit(x_train, y_train) 
+print("Training Completed")
